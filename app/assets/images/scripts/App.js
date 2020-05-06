c7cff0588ffe27299dc84540cdc3347f3da66c3d
snufkin <myrm.nn@gmail.com>
2020-05-06 12:17:55 -0700

    finished header

--- app/assets/images/scripts/App.js
+++ app/assets/images/scripts/App.js
@@ -1,9 +1,11 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/mobile-menu'
 import RevealOnScroll from './modules/reveal-on-scroll'
+import StickyHeader from './modules/sticky-header'
 
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
+let stickyHeader = new StickyHeader();
 
 let mobileMenu = new MobileMenu();
 
