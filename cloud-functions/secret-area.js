277cc6e57bde9c029392b85e8fead6570f90334e
myrmnn <myrm.nn@gmail.com>
2020-05-28 00:21:03 -0400

    updated cloud funuction to check for password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,26 @@
 exports.handler = function (event, context, callback) {
-callback(null, {
-    statusCode: 200,
-    body: "Welcome to the secret area!"
-})
+    const secretContent = `
+    <h3>Welcome to the secret area!</3>
+    <p> Dublin is the cutest cat in the entire world. That is all.</p>
+    `
+let body
+
+if(event.body){
+ body = JSON.parse(event.body)
+} else {
+    body ={}
+}
+
+if(body.password == "javascript"){
+    callback(null, {
+        statusCode: 200,
+        body: "Welcome to the secret area!"
+    })
+} else{
+    callback(null, {
+        statusCode: 401,
+    })
+}
+
+
 }
