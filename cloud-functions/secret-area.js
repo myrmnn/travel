6c157d736acb08b294b21859b787c6f5d66a4f80
snufkin <myrm.nn@gmail.com>
2020-10-19 14:38:15 -0700

    edit to test cloud function

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,26 +1,33 @@
 exports.handler = function (event, context, callback) {
-    const secretContent = `
-    <h3>Welcome to the secret area!</3>
-    <p>The secret is that Dublin is the cutest cat in the entire world. That is all.</p>
-    `
-let body
-
-if(event.body){
- body = JSON.parse(event.body)
-} else {
-    body ={}
-}
-
-if(body.password == "javascript"){
     callback(null, {
         statusCode: 200,
-        body: secretContent
-    })
-} else{
-    callback(null, {
-        statusCode: 401,
+        body: 'Welcome to the secret area'
     })
 }
 
+// exports.handler = function (event, context, callback) {
+//     const secretContent = `
+//     <h3>Welcome to the secret area!</3>
+//     <p>The secret is that Dublin is the cutest cat in the entire world. That is all.</p>
+//     `
+// let body
 
-}
+// if(event.body){
+//  body = JSON.parse(event.body)
+// } else {
+//     body ={}
+// }
+
+// if(body.password == "javascript"){
+//     callback(null, {
+//         statusCode: 200,
+//         body: secretContent
+//     })
+// } else{
+//     callback(null, {
+//         statusCode: 401,
+//     })
+// }
+
+
+// }
