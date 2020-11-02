ed16c8a8c5bb5ba2df6a7061851fccc853698b8c
myrmnn <myrm.nn@gmail.com>
2020-11-02 08:10:24 -0500

    added password to page

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,26 +1,24 @@
 exports.handler = function (event, context, callback) {
-    const secretContent = `
+  const secretContent = `
     <h3>Welcome to the secret area!</3>
     <p>The secret is that Dublin is the cutest cat in the entire world.</p>
-    `
-let body
+    `;
+  let body;
 
-if(event.body){
- body = JSON.parse(event.body)
-} else {
-    body ={}
-}
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    body = {};
+  }
 
-if(body.password == "javascript"){
+  if (body.password == "javascript") {
     callback(null, {
-        statusCode: 200,
-        body: secretContent
-    })
-} else{
+      statusCode: 200,
+      body: secretContent,
+    });
+  } else {
     callback(null, {
-        statusCode: 401
-    })
-}
-
-
-}
+      statusCode: 401,
+    });
+  }
+};
